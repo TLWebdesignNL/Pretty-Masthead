13b8c64db5d0774cc52c6638eb1d04113fa6c936
TLWebdesign <info@tlwebdesign.nl>
2022-08-18 15:36:15 +0200

    typo

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  com_prettymasthead
+ * @subpackage  mod_prettymasthead
  *
  * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
